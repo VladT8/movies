4e398885f67180129a358d5a8b5df0aa72b1bed3
VladT8 <tishuk.vladzislau@gmail.com>
2023-09-22 21:09:25 +0300

    + themeContext

---
+++ movies/src/components/themeContext/index.ts
@@ -0,0 +1,11 @@
+import React from "react"
+import { Theme, ThemeType } from "./themes"
+
+export const initialContext = {}
+
+interface ThemeContextProps {
+   currentTheme: ThemeType;
+   stylesForTheme: Theme;
+}
+
+export const ThemeContext = React.createContext({} as ThemeContextProps)

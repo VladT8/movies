630a1add4fad54a2855fc6f4f9f326b3deca42f3
VladT8 <tishuk.vladzislau@gmail.com>
2023-10-30 12:43:30 +0300

    add styles for single movie page

--- movies/src/components/ui/singlePost/singleMoviePage.styles.tsx
+++ movies/src/components/ui/singlePost/singleMoviePage.styles.tsx
@@ -9,64 +9,79 @@ interface SingleMoviePageStyleProps {
 export const SingleMoviePageStyle = styled.div<SingleMoviePageStyleProps>`
   width: 100%;
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
   padding: 16px;
   background-color: ${({ theme }) => theme?.background};
   color: ${({ theme }) => theme?.text};
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+  }
 `;
 export const SingleMoviePageStylePoster = styled.div<SingleMoviePageStyleProps>`
-  width: 200px;
-  height: 500px;
+  width: 40%;
+  height: auto;
+  margin-right: 25px;
+  @media (max-width: 768px) {
+    margin: 0 auto;
+  }
   img {
     height: 100%;
     width: 100%;
   }
 `;
 export const SingleMoviePageStyleTitle = styled.h2<SingleMoviePageStyleProps>`
-  font-size: 24px;
+  font-size: 30px;
   font-weight: 600;
   color: ${({ theme }) => theme?.text};
+  margin-bottom: 15px;
 `;
 
 export const SinglePostinfo = styled.h3<SingleMoviePageStyleProps>`
-  font-size: 16px;
+  font-size: 18px;
   margin: 0;
   padding: 0;
   color: ${({ theme }) => theme?.text};
+  margin-bottom: 10px;
 `;
 export const SinglePostPlot = styled.h2<SingleMoviePageStyleProps>`
-  font-size: 16px;
+  font-size: 18px;
   margin: 0;
   padding: 0;
   color: ${({ theme }) => theme?.text};
+  margin-bottom: 10px;
 `;
 export const SinglePostDirector = styled.h2<SingleMoviePageStyleProps>`
-  font-size: 16px;
+  font-size: 18px;
   margin: 0;
   padding: 0;
   color: ${({ theme }) => theme?.text};
+  margin-bottom: 10px;
 `;
 export const SinglePostActors = styled.h2<SingleMoviePageStyleProps>`
-  font-size: 16px;
+  font-size: 18px;
   margin: 0;
   padding: 0;
   color: ${({ theme }) => theme?.text};
+  margin-bottom: 10px;
 `;
 export const SinglePostCountry = styled.h2<SingleMoviePageStyleProps>`
-  font-size: 16px;
+  font-size: 18px;
   margin: 0;
   padding: 0;
   color: ${({ theme }) => theme?.text};
+  margin-bottom: 10px;
 `;
 export const SinglePostAwards = styled.h2<SingleMoviePageStyleProps>`
-  font-size: 16px;
+  font-size: 18px;
   margin: 0;
   padding: 0;
   color: ${({ theme }) => theme?.text};
+  margin-bottom: 10px;
 `;
 export const SinglePostimdbRating = styled.h2<SingleMoviePageStyleProps>`
-  font-size: 16px;
+  font-size: 18px;
   margin: 0;
   padding: 0;
   color: ${({ theme }) => theme?.text};
+  margin-bottom: 10px;
 `;
